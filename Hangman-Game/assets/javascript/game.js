e496be20260daf050172c7e6119758d7d6fe1224
Cassie Rabe <cjrabe17@gmail.com>
2017-11-23 22:21:07 -0500

    Game functionality updated

--- Hangman-Game/assets/javascript/game.js
+++ Hangman-Game/assets/javascript/game.js
@@ -1,11 +1,10 @@
 
-words = ["mario", "computer", "hangman", "test", "antidisestablishmentarianism"];
-
+words = ["zork", "mario", "portal", "pacman", "halflife", "minecraft", "borderlands"];
+aryGuess = [];
 winCounter = 0;
-document.getElementById("winCounter").innerHTML = winCounter;
-
+// document.getElementById("wins").innerHTML = winCounter;
 remainingGuesses = 13;
-document.getElementById("remainingGuesses").innerHTML = remainingGuesses;
+// document.getElementById("guesses").innerHTML = remainingGuesses;
 
 // Picks a random word from those available in the array
 var wordToGuess = words[Math.floor(Math.random() * words.length)];
@@ -13,10 +12,10 @@ var wordToGuess = words[Math.floor(Math.random() * words.length)];
 	console.log(wordToGuess.length);
 
 // Displays underscores for the # of letters of the chosen word
-var answerArray = [];
+aryPositions = [];
 for (var i = 0; i < wordToGuess.length; i++) {
-	answerArray[i] = "_";
-	underscores.innerHTML = answerArray.join(" ");
+	aryPositions[i] = "_";
+	underscores.innerHTML = aryPositions.join(" ");
 }
 
 // Listens for the user's key press and converts it to lowercase
@@ -25,22 +24,30 @@ document.onkeypress = function(event) {
 	console.log(userKeyPressed);
 	console.log(wordToGuess.indexOf(userKeyPressed));
 
-aryPositions = [];
-aryGuess = [];
-
 // If the letter is in the word
 if (wordToGuess.indexOf(userKeyPressed) > -1) {
 	for (var i = 0; i < wordToGuess.length; i++) {
 		if (wordToGuess[i] == userKeyPressed) {
-			// aryPositions[i] == userKeyPressed;
-		} else {
-			remainingGuesses--;
-			aryGuess.push(userKeyPressed);
-			console.log(aryGuess);
+			aryPositions[i] = userKeyPressed;
+			console.log(aryPositions);
+			underscores.innerHTML = aryPositions.join(" ");
 		}
 	}
-}
-
+// If the letter isn't in the word
+} else {
+	remainingGuesses--;
+	guesses.innerHTML = remainingGuesses;
+	console.log(aryGuess);
+	aryGuess.push(userKeyPressed);
+	console.log(aryGuess);
+	wrong.innerHTML = aryGuess.join(" ");
+		// When remaining guesses run out
+		if (remainingGuesses == 0) {
+			document.getElementById("wrong").innerHTML = "You lose.";
+			console.log("You made it.")
+			// reset();
+		}
+	}
 }
 
 // var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
