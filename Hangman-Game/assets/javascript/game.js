d0d1f9abb04751ab1ed565adf8810b0114877026
Cassie Rabe <cjrabe17@gmail.com>
2017-11-29 22:02:34 -0500

    Updated audio code

--- Hangman-Game/assets/javascript/game.js
+++ Hangman-Game/assets/javascript/game.js
@@ -1,7 +1,18 @@
 
-words = ["zork", "mario", "portal", "pacman", "halflife", "minecraft", "borderlands"];
-aryGuess = [];
-aryPositions = [];
+var alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var words = ["zork", "mario", "portal", "pacman", "halflife", "minecraft", "borderlands"];
+var aryGuess = [];
+var aryPositions = [];
+var audio = document.createElement("audio");
+var songs = {
+	mario: "assets/audio/supermariobros.mp3",
+	portal:"assets/audio/stillalive.mp3",
+	zork: "assets/audio/zork.mp3",
+	pacman: "assets/audio/pacman.mp3",
+	halflife: "assets/audio/halflife.mp3",
+	minecraft: "assets/audio/minecraft.mp3",
+	borderlands: "assets/audio/borderlands.mp3",
+}
 
 function showWins() {
 	winCounter = 0;
@@ -46,21 +57,35 @@ function checkWin() {
 		var image = document.getElementById("image");
 		if (wordToGuess == "mario") {
 			image.src = "assets/images/mario.png";
+			audio.src = songs.mario;
+			audio.play();
 		} else if (wordToGuess == "portal") {
 			image.src = "assets/images/portal.png";
+			audio.src = songs.portal;
+			audio.play();
 		} else if (wordToGuess == "zork") {
 			image.src = "assets/images/zork.jpg";
+			audio.src = songs.zork;
+			audio.play();
 		} else if (wordToGuess == "pacman") {
 			image.src = "assets/images/pacman.jpg";
+			audio.src = songs.pacman;
+			audio.play();
 		} else if (wordToGuess == "halflife") {
 			image.src = "assets/images/halflife.png";
+			audio.src = songs.halflife;
+			audio.play();
 		} else if (wordToGuess == "minecraft") {
 			image.src = "assets/images/minecraft.png";
-		} else if (wordToGuess == "borderlands") {
+			audio.src = songs.minecraft;
+			audio.play();
+		} else {
+			// (wordToGuess == "borderlands")
 			image.src = "assets/images/borderlands.png";
+			audio.src = songs.borderlands;
+			audio.play();
 		}
-		setTimeout(newGame, 2000);
-		// newGame();
+		setTimeout(newGame, 1500);
 	}
 }
 
@@ -72,7 +97,7 @@ function checkLoss() {
 		image.src = "assets/images/sadclaptrap.jpg";
 		var audio = new Audio("assets/audio/lose.mp3");
 		audio.play();
-		newGame();
+		setTimeout(newGame, 1500);
 	}
 }
 
@@ -88,23 +113,31 @@ showRemainingGuesses();
 document.onkeypress = function(event) {
 	var userKeyPressed = String.fromCharCode(event.keyCode).toLowerCase();
 
-// If the letter is in the word
-if (wordToGuess.indexOf(userKeyPressed) > -1) {
-	for (var i = 0; i < wordToGuess.length; i++) {
-		if (wordToGuess[i] == userKeyPressed) {
-			aryPositions[i] = userKeyPressed;
-			underscores.innerHTML = aryPositions.join(" ");
+
+// Checks if user key pressed is an actual letter
+if (alpha.indexOf(userKeyPressed) > -1) {
+	// If the letter is in the word
+	if (wordToGuess.indexOf(userKeyPressed) > -1) {
+		for (var i = 0; i < wordToGuess.length; i++) {
+			if (wordToGuess[i] == userKeyPressed) {
+				aryPositions[i] = userKeyPressed;
+				underscores.innerHTML = aryPositions.join(" ");
+			}
 		}
+	} else if (aryGuess.includes(userKeyPressed)) {
+		// If the key has been pressed before, nothing will happen
+	} else {
+		// If the letter isn't in the word
+		remainingGuesses--;
+		guesses.innerHTML = remainingGuesses;
+		aryGuess.push(userKeyPressed);
+		wrong.innerHTML = aryGuess.join(" ");
 	}
-} else if (aryGuess.includes(userKeyPressed)) {
-	// If the wrong key has been pressed before, nothing will happen
+	checkLoss();
+	checkWin();
 } else {
-	// If the letter isn't in the word
-	remainingGuesses--;
-	guesses.innerHTML = remainingGuesses;
-	aryGuess.push(userKeyPressed);
-	wrong.innerHTML = aryGuess.join(" ");
+	// Do nothing because the directions explicitly say to press a letter!
 }
-checkLoss();
-checkWin();
+
+
 }
