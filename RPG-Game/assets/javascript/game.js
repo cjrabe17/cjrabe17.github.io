0c7837e3c1332e38ddb66d56288f2757900923c1
Cassie Rabe <cjrabe17@gmail.com>
2017-12-02 18:00:45 -0500

    Initial RPG files

---
+++ RPG-Game/assets/javascript/game.js
@@ -0,0 +1,47 @@
+// Global variables
+var gator = {
+	name: Florida Gators,
+	health: 150,
+	counterAttack: 25,
+	attack: function() {
+		Math.floor((Math.random() * 50) + 1);
+	}
+}
+
+var noles = {
+	name: Florida State Seminoles,
+	health: 80,
+	counterAttack: 10,
+	attack: function() {
+		Math.floor((Math.random() * 50) + 1);
+	}
+}
+
+var duke = {
+	name: Duke Blue Devils,
+	health: 120,
+	counterAttack: 20,
+	attack: function() {
+		Math.floor((Math.random() * 50) + 1);
+	}
+}
+
+var kentucky = {
+	name: Kentucky Wildcats,
+	health: 100,
+	counterAttack: 15,
+	attack: function() {
+		Math.floor((Math.random() * 50) + 1);
+	}
+}
+
+
+
+$(document).ready(function() {
+
+
+	$(".attack-button").on("click", function() {
+		// if attacker and defender have been chosen
+			// attackerHP-defenderAttack
+	});
+}
