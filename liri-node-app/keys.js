c6c9766155c2e1a42b9a9a8b1e59866308971d5d
Cassie <cjrabe17@gmail.com>
2018-01-27 16:55:51 -0500

    Twitter functionality fixed

--- liri-node-app/keys.js
+++ liri-node-app/keys.js
@@ -1,13 +1,13 @@
-console.log("This is loaded!");
+console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.b0oznhZwjbagwI0FgPh69CJSt,
-  consumer_secret: process.env.cKnvKsXY6uz2SmUI1OOYvyhlfgMgFe4N1oFsWh9igvlnMFMMYy,
-  access_token_key: process.env.UxKgWncPFlkVnkIV0ZNtJFyncqHuO4Q,
-  access_token_secret: process.env.H9U3KoFfO4SblIFCH2IRAln0eFDniwXYuR2VS9BaDRkLm
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
-  id: process.env.c02084acfe2148a8ada6e1ec8167bbfc,
-  secret: process.env.f985eb8112f14746a4db51c0f07c775e
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
--- liri-node-app/liri.js
+++ liri-node-app/liri.js
@@ -1,27 +1,23 @@
 require("dotenv").config();
-
 var keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
+var Twitter = require('twitter');
+var Spotify = require("spotify");
+ 
 var client = new Twitter(keys.twitter);
 
+var nodeArgv = process.argv;
 var command = process.argv[2];
-
-
+var searchTerm = process.argv[3];
+ 
+var params = {screen_name: 'cjrabe17'};
 if (command == "my-tweets") {
-    client.get("statuses/user_timeline", params, function(error, tweets, response) {
-        if (error) {
-            console.log(error);
-        } else {
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
             console.log(tweets);
         }
     });
-} else if (command == "spotify-this-song") {
-    // do something
-} else if (command == "movie-this") {
-    // do something
-} else if (command == "do-what-it-says") {
-    // do something
-} else {
-    console.log("Please type a command in the correct format.");
-}
+} else if (command == "spotify-this-song")
+
+
+// // var spotify = new Spotify(keys.spotify);
