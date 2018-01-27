d3a96a0e9f2f38b99f78e76b8ba4441541eaf866
Cassie <cjrabe17@gmail.com>
2018-01-27 16:02:00 -0500

    Removed HTML file, updated cmd arg if-else statement

--- liri-node-app/liri.js
+++ liri-node-app/liri.js
@@ -1,1 +1,27 @@
-console.log("Test");
+require("dotenv").config();
+
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+var command = process.argv[2];
+
+
+if (command == "my-tweets") {
+    client.get("statuses/user_timeline", params, function(error, tweets, response) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(tweets);
+        }
+    });
+} else if (command == "spotify-this-song") {
+    // do something
+} else if (command == "movie-this") {
+    // do something
+} else if (command == "do-what-it-says") {
+    // do something
+} else {
+    console.log("Please type a command in the correct format.");
+}
