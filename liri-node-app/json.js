92821f75f6e02acbde7dce28e92daf5e69adf0e1
Cassie <cjrabe17@gmail.com>
2018-01-28 22:14:18 -0500

    Added Twitter, Spotify & OMDb API functionality

---
+++ liri-node-app/json.js
@@ -0,0 +1,75 @@
+{
+  "tracks" : {
+    "href" : "https://api.spotify.com/v1/search?query=my+band&type=track&market=US&offset=0&limit=1",
+    "items" : [ {
+      "album" : {
+        "album_type" : "album",
+        "artists" : [ {
+          "external_urls" : {
+            "spotify" : "https://open.spotify.com/artist/5Qi4Bb7a8C0a00NZcA77L0"
+          },
+          "href" : "https://api.spotify.com/v1/artists/5Qi4Bb7a8C0a00NZcA77L0",
+          "id" : "5Qi4Bb7a8C0a00NZcA77L0",
+          "name" : "D12",
+          "type" : "artist",
+          "uri" : "spotify:artist:5Qi4Bb7a8C0a00NZcA77L0"
+        } ],
+        "available_markets" : [ "AD", "AR", "AT", "AU", "BE", "BG", "BO", "BR", "CA", "CH", "CL", "CO", "CR", "CY", "CZ", "DE", "DK", "DO", "EC", "EE", "ES", "FI", "FR", "GB", "GR", "GT", "HK", "HN", "HU", "ID", "IE", "IS", "IT", "JP", "LI", "LT", "LU", "LV", "MC", "MT", "MX", "MY", "NI", "NL", "NO", "NZ", "PA", "PE", "PH", "PL", "PT", "PY", "SE", "SG", "SK", "SV", "TH", "TR", "TW", "US", "UY", "VN" ],
+        "external_urls" : {
+          "spotify" : "https://open.spotify.com/album/61EgDesQnFVyYf2UneM9PQ"
+        },
+        "href" : "https://api.spotify.com/v1/albums/61EgDesQnFVyYf2UneM9PQ",
+        "id" : "61EgDesQnFVyYf2UneM9PQ",
+        "images" : [ {
+          "height" : 635,
+          "url" : "https://i.scdn.co/image/0b1709839523d5797e011ffd13fd57a320a5e59f",
+          "width" : 640
+        }, {
+          "height" : 297,
+          "url" : "https://i.scdn.co/image/adbb984752cd294c4b7f5e96b689ac7bb68c22a8",
+          "width" : 300
+        }, {
+          "height" : 63,
+          "url" : "https://i.scdn.co/image/038f230cebda5e10dba789196da536729c884486",
+          "width" : 64
+        } ],
+        "name" : "D-12 World",
+        "type" : "album",
+        "uri" : "spotify:album:61EgDesQnFVyYf2UneM9PQ"
+      },
+      "artists" : [ {
+        "external_urls" : {
+          "spotify" : "https://open.spotify.com/artist/5Qi4Bb7a8C0a00NZcA77L0"
+        },
+        "href" : "https://api.spotify.com/v1/artists/5Qi4Bb7a8C0a00NZcA77L0",
+        "id" : "5Qi4Bb7a8C0a00NZcA77L0",
+        "name" : "D12",
+        "type" : "artist",
+        "uri" : "spotify:artist:5Qi4Bb7a8C0a00NZcA77L0"
+      } ],
+      "available_markets" : [ "AD", "AR", "AT", "AU", "BE", "BG", "BO", "BR", "CA", "CH", "CL", "CO", "CR", "CY", "CZ", "DE", "DK", "DO", "EC", "EE", "ES", "FI", "FR", "GB", "GR", "GT", "HK", "HN", "HU", "ID", "IE", "IS", "IT", "JP", "LI", "LT", "LU", "LV", "MC", "MT", "MX", "MY", "NI", "NL", "NO", "NZ", "PA", "PE", "PH", "PL", "PT", "PY", "SE", "SG", "SK", "SV", "TH", "TR", "TW", "US", "UY", "VN" ],
+      "disc_number" : 1,
+      "duration_ms" : 298773,
+      "explicit" : true,
+      "external_ids" : {
+        "isrc" : "USIR10400103"
+      },
+      "external_urls" : {
+        "spotify" : "https://open.spotify.com/track/4XHQyvbrBsQaaBUW1VvmsL"
+      },
+      "href" : "https://api.spotify.com/v1/tracks/4XHQyvbrBsQaaBUW1VvmsL",
+      "id" : "4XHQyvbrBsQaaBUW1VvmsL",
+      "name" : "My Band",
+      "popularity" : 64,
+      "preview_url" : null,
+      "track_number" : 6,
+      "type" : "track",
+      "uri" : "spotify:track:4XHQyvbrBsQaaBUW1VvmsL"
+    } ],
+    "limit" : 1,
+    "next" : "https://api.spotify.com/v1/search?query=my+band&type=track&market=US&offset=1&limit=1",
+    "offset" : 0,
+    "previous" : null,
+    "total" : 29182
+  }
+}
--- liri-node-app/liri.js
+++ liri-node-app/liri.js
@@ -1,23 +1,68 @@
-require("dotenv").config();
-var keys = require("./keys.js");
-
+require('dotenv').config();
+var keys = require('./keys.js');
+var request = require('request');
 var Twitter = require('twitter');
-var Spotify = require("spotify");
- 
+var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+var params = {screen_name: 'cjrabe17'};
 
 var nodeArgv = process.argv;
 var command = process.argv[2];
 var searchTerm = process.argv[3];
- 
-var params = {screen_name: 'cjrabe17'};
-if (command == "my-tweets") {
+for (i = 4; i < nodeArgv.length; i++) {
+    searchTerm += '+' + nodeArgv[i];
+}
+
+// Limit tweets to 20
+if (command == 'my-tweets') {
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (!error) {
-            console.log(tweets);
+            for (i = 0; i < tweets.length; i++) {
+                var date = tweets[i].created_at;
+                console.log(date.substring(0, 10) + ':\n' + tweets[i].text + '\n-----------------------');
+            }
+        } else {
+            console.log(error);
         }
     });
-} else if (command == "spotify-this-song")
+} else if (command == 'spotify-this-song') {
+    if (searchTerm !== undefined) {
+        spotify.search({type: 'track', query: searchTerm, limit: 1}, function(err, data) {
+            if (!err) {
+                console.log('Artist: ' + data.tracks.items[0].artists[0].name + '\nSong Title: ' + data.tracks.items[0].name + '\nAlbum Name: ' + data.tracks.items[0].album.name + '\nPreview Link: ' + data.tracks.items[0].album.external_urls.spotify);
+            } else {
+                console.log(err);
+            }
+        });
+    } else {
+        spotify.search({type: 'track', query: 'the sign', limit: 1}, function(err, data) {
+            if (!err) {
+                console.log('Artist: ' + data.tracks.items[0].artists[0].name + '\nSong Title: ' + data.tracks.items[0].name + '\nAlbum Name: ' + data.tracks.items[0].album.name + '\nPreview Link: ' + data.tracks.items[0].album.external_urls.spotify);
+            } else {
+                console.log(err);
+            }
+        });
+    }
+} else if (command == 'movie-this') {
+    if (searchTerm !== undefined) {
+        request('http://www.omdbapi.com/?t=' + searchTerm + '&plot=short&apikey=trilogy', function(error, response, body) {
+            if (!error && response.statusCode === 200) {
+                var parsedMovieData = JSON.parse(body);
+                console.log('Title: ' + parsedMovieData.Title + '\nRelease Year: ' + parsedMovieData.Year + '\nIMDb Rating: ' + parsedMovieData.imdbRating + '\nRotten Tomatoes Rating: ' + parsedMovieData.Ratings[1].Value + '\nCountry of Production: ' + parsedMovieData.Country + '\nLanguage: ' + parsedMovieData.Language + '\nPlot: ' + parsedMovieData.Plot + '\nActors: ' + parsedMovieData.Actors);
+            }
+        });
+    } else {
+        request('http://www.omdbapi.com/?t=mr+nobody&plot=short&apikey=trilogy', function(error, response, body) {
+            if (!error && response.statusCode === 200) {
+                var parsedMovieData = JSON.parse(body);
+                console.log('Title: ' + parsedMovieData.Title + '\nRelease Year: ' + parsedMovieData.Year + '\nIMDb Rating: ' + parsedMovieData.imdbRating + '\nRotten Tomatoes Rating: ' + parsedMovieData.Ratings[1].Value + '\nCountry of Production: ' + parsedMovieData.Country + '\nLanguage: ' + parsedMovieData.Language + '\nPlot: ' + parsedMovieData.Plot + '\nActors: ' + parsedMovieData.Actors);
+            }
+        });
+    }
+} else if (command == 'do-what-it-says') {
 
-
-// // var spotify = new Spotify(keys.spotify);
+} else {
+    console.log('Please type an approved command.');
+}
