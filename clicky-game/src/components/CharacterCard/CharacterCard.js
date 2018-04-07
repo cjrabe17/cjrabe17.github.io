6e2b7b4feaf089972893a837c1582785ee28603f
cjrabe17 <cjrabe17@gmail.com>
2018-04-06 20:54:53 -0400

    trying to get cards to display inline

--- clicky-game/src/components/CharacterCard/CharacterCard.js
+++ clicky-game/src/components/CharacterCard/CharacterCard.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./CharacterCard.css";
 
 const CharacterCard = props => (
-    <img alt={props.name} src={props.image} className="img-responsive" />
+  <div>
+      <a onClick={() => props.selectCharacter(props.character)} className={props.currentScore === 0}>
+        <img alt={props.name} src={props.image} className="img-responsive" />
+      </a>
+  </div>
 );
 
 export default CharacterCard;
